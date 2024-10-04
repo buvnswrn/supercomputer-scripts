198a4682db23294fb610a85fddec7c9ce71ab125
Bhuvaneshwaran Ilanthirayan <bhuvan.nav@gmail.com>
2024-10-04 17:22:44 +0200

    Updated the TEI script

--- tei_server.sh
+++ tei_server.sh
@@ -16,10 +16,15 @@ export NUMBA_CACHE_DIR=/tmp
 export NUMBA_DEBUG_CACHE=1
 export NCCL_DEBUG=INFO
 #PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("",0)); print(s.getsockname()[1]);s.close()')
-#NODE_IP=$(hostname --ip-address)
+NODE_IP=$(hostname --ip-address)
 
 
-MODEL_REPO="BAAI/bge-large-en-v1.5"
+MODEL_REPO="Alibaba-NLP/gte-Qwen2-7B-instruct"
+TGI_SERVER_IP=$(cat ~/tgi_server_ip.env)
+echo "TGI Server is at: $TGI_SERVER_IP"
+CLUSTER_REGISTER_ENDPOINT="http://$TGI_SERVER_IP:8000/api/v1/other_services/register_cluster"
+echo "Registering Server at $CLUSTER_REGISTER_ENDPOINT"
+
 HF_TOKEN=$(cat ~/hf_auth.env)
 APPTAINER="apptainer run --nvccli -B ./data/:/data --env HF_TOKEN=$HF_TOKEN "
 CONTAINER="text-embeddings-inference_1.5.sif"
@@ -30,5 +35,6 @@ echo "HEAD NODE: $(hostname)"
 echo "IP ADDRESS: $(hostname --ip-address)"
 #echo "Using port: $NODE_PORT on node $SLURM_NODEID"
 echo "SSH TUNNEL (HTTP): ssh -p 8822 ${USER}@login.lxp.lu -NL 8002:$(hostname --ip-address):8080"
+curl -X POST "$CLUSTER_REGISTER_ENDPOINT?ip=$NODE_IP"
 
 srun ${APPTAINER} ${CONTAINER} ${TEI}
